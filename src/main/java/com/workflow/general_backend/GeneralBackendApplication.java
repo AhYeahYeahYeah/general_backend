60108ca65c6c7280d7bb0d76bd29a5217b0a1b4e
cutanojiang <cutanojiang@tencent.com>
2022-02-23 15:51:25 +0800

    Initial commit

---
+++ src/main/java/com/workflow/general_backend/GeneralBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.general_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workflow/general_backend/GeneralBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.general_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
