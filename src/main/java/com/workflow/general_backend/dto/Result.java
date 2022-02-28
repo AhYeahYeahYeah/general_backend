19a85c82c0220a7b3c0e73484086cc1716b2f62c
chooper666 <1721455274@qq.com>
2022-02-28 11:25:35 +0800

    add result class

---
+++ src/main/java/com/workflow/general_backend/dto/Result.java
@@ -0,0 +1,22 @@
+package com.workflow.general_backend.dto;
+
+public class Result {
+    String status;
+    String msg;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
