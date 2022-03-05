7e045558b9951a0324d07711dccf8afa812e7fc6
chooper666 <1721455274@qq.com>
2022-03-05 16:12:00 +0800

    add column and table

---
+++ src/main/java/com/workflow/general_backend/entity/Permission.java
@@ -0,0 +1,22 @@
+package com.workflow.general_backend.entity;
+
+public class Permission {
+    private String pid;
+    private String perm;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getPerm() {
+        return perm;
+    }
+
+    public void setPerm(String perm) {
+        this.perm = perm;
+    }
+}
