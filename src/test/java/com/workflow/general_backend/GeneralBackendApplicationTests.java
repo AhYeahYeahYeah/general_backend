936c3ab979cc0bec9094b14ef8fc4aa4571f4e2a
ambrosecm <3021572573@qq.com>
2022-03-14 17:50:28 +0800

    Update GeneralBackendApplicationTests.java

--- src/test/java/com/workflow/general_backend/GeneralBackendApplicationTests.java
+++ src/test/java/com/workflow/general_backend/GeneralBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.workflow.general_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAsync
 class GeneralBackendApplicationTests {
 
     @Test
