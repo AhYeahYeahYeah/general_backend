7e3f69bdabe40c56200957c0ed53fd08fc4d3123
chooper666 <1721455274@qq.com>
2022-03-15 21:34:28 +0800

    new Room class

---
+++ src/main/java/com/workflow/general_backend/entity/Room.java
@@ -0,0 +1,44 @@
+package com.workflow.general_backend.entity;
+
+import cn.hutool.db.Session;
+
+import java.util.List;
+
+public class Room {
+    String sid;
+    String name;
+    String password;
+    List<String> accountList;
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(List<String> accountList) {
+        this.accountList = accountList;
+    }
+}
